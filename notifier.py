beeff44925b55ac104cf77bb5009c0edfef3b59f
PotionOfHarming <PotOfHarming@gmail.com>
2025-12-16 23:06:03 +0100

    --no-message

--- notifier.py
+++ notifier.py
@@ -1,20 +1,6 @@
 import firebase_admin, os, json
 from firebase_admin import credentials, messaging
 
-# credentials file location
-BASE_PATH = os.path.dirname(__file__)
-with open(os.path.join(BASE_PATH, "config.json"), 'r') as f:
-    configFile = json.load(f)
-    CRED_FILE = configFile["firebase_file_path"]
-
-# initialize credentials
-cred = credentials.Certificate(CRED_FILE)
-firebase_admin.initialize_app(cred)
-if __name__ == "__main__":
-    print(CRED_FILE)
-    with open(CRED_FILE, 'r') as c:
-        print("Project ID: ", json.load(c)["project_id"])
-
 # send message
 def sendMessage(msgTitle, msgBody, msgImg=None, msgTopic="all"):
     msg = messaging.Message(
@@ -29,8 +15,19 @@ def sendMessage(msgTitle, msgBody, msgImg=None, msgTopic="all"):
 
     messaging.send(msg)
 
-# sendMessage("Boom", "Je bent een boom van een kerel", "https://hetbosderomarming.nl/wp-content/uploads/2020/09/47439162_s.jpg")
-if __name__ == '__name__':
+# credentials file location
+BASE_PATH = os.path.dirname(__file__)
+with open(os.path.join(BASE_PATH, "config.json"), 'r') as f:
+    configFile = json.load(f)
+    CRED_FILE = configFile["firebase_file_path"]
+
+# initialize credentials
+cred = credentials.Certificate(CRED_FILE)
+firebase_admin.initialize_app(cred)
+if __name__ == "__main__":
+    print(CRED_FILE)
+    with open(CRED_FILE, 'r') as c:
+        print("Project ID: ", json.load(c)["project_id"])
     title: str = str(input("Input a title: "))
     desc: str = str(input("Input a description: "))
     img: str = str(input("Input the url of an image (leave empty if none)"))
@@ -38,3 +35,6 @@ if __name__ == '__name__':
         sendMessage(title, desc)
     else:
         sendMessage(title, desc, img)
+
+
+# sendMessage("Boom", "Je bent een boom van een kerel", "https://hetbosderomarming.nl/wp-content/uploads/2020/09/47439162_s.jpg")
